6b28a204b5176306d97ac3cc51b4502d256e4f94
starberdine <45822657+starberdine@users.noreply.github.com>
2018-12-12 10:51:51 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "The Champ";
 
   options = {
     hostname: 'api.groupme.com',
