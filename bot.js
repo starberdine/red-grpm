2494f8b3230a1541869687109b0a7673c3f3bb7f
starberdine <45822657+starberdine@users.noreply.github.com>
2019-01-04 16:41:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 var hunting = 1;
 function sleep (time) {
-  return new Promise((resolve);
+  return new Promise((resolve)
   setTimeout(resolve, time));
 }
 //var pokemon = [name,level];
